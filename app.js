eb843eafbbe0fa6b84d3ae1c96d0a842c10121ab
Serjio31 <s.kamnev@yandex.ru>
2018-11-29 21:30:01 +0300

    delete temp files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const Post = require('./models/post');
 
@@ -7,26 +8,18 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    '/javascripts',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
 
 
 app.get('/', (req, res) => {
   Post.find({}).then(posts => {
       res.render('index', {posts: posts});
   });
-});
-
-app.get('/create', (req, res) => res.render('create'));
-
-app.post('/create', (req, res) => {
-
-    const {title, body} = req.body;
-
-    Post.create({
-        title: title,
-        body: body
-    }).then(post => console.log(post.id));
-
-    res.redirect('/');
 });
 
 module.exports = app;
